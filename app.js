93f114301d8a694588cd563ddd43326a702c3578
Eshy10 <adewalebose15@gmail.com>
2021-12-03 01:31:41 +0100

    add swagger.yaml

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express');
 const app = express();
 
@@ -36,6 +41,12 @@ app.use(xss());
 // extra packages
 
 // routes
+
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', userAuthentication, jobRouter)
 
